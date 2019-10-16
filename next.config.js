ab17dc82fd3bde132d64fe6d40b45bd546a80598
Richard Lynch <lynchardrich@gmail.com>
2019-10-16 20:48:13 +0100

    faster dev

--- next.config.js
+++ next.config.js
@@ -1,22 +1,42 @@
-require('dotenv').config()
-const withCSS = require('@zeit/next-css')
-const path = require('path')
-const Dotenv = require('dotenv-webpack')
+require("dotenv").config();
+const withCSS = require("@zeit/next-css");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const withPlugins = require("next-compose-plugins");
+const webpack = require('webpack')
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
-module.exports = withCSS({
-  webpack: config => {
-    config.plugins = config.plugins || []
+const nextConfig = {
+  distDir: ".next",
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  },
+  webpack: (config, options) => {
+    config.plugins = config.plugins || [];
 
     config.plugins = [
       ...config.plugins,
 
       // Read the .env file
       new Dotenv({
-        path: path.join(__dirname, '.env'),
+        path: path.join(__dirname, ".env"),
         systemvars: true
-      })
-    ]
+      }),
 
-    return config
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ];
+
+    return config;
   }
-})
+};
+
+module.exports = withPlugins([[withCSS], [withBundleAnalyzer]], nextConfig);
